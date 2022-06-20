78d30e315dc3fc298c869babc2f9c2e16950fcbc
xiaoyunpeng <xiaoyunpeng01@qianxin.com>
2022-06-20 19:52:29 +0800

    feat: 进一步深入特征

---
+++ src/bin/trait_plus/main.rs
@@ -0,0 +1,73 @@
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Millimeters(u32);
+#[derive(Debug, PartialEq)]
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, rhs: Meters) -> Self::Output {
+        Millimeters(self.0 + rhs.0)
+    }
+}
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("Pilot::fly");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Wizard::fly");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("Human::fly");
+    }
+}
+
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    assert_eq!(Millimeters(1) + Meters(1), Millimeters(2));
+
+    let human = Human;
+    human.fly();
+    Pilot::fly(&human);
+    Wizard::fly(&human);
+}
